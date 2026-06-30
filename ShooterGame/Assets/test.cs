7a399f9b07c1d2f4c496e1770e2e02a54eb63eb5
user.name "Manaal Fatima <manaalfatima37@gmail.com>
2026-06-30 17:35:45 +0500

    test commit

---
+++ ShooterGame/Assets/test.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class GitTest : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Git collaboration test successful!");
+    }
+
+    void Update()
+    {
+        // This script is only for testing Git collaboration.
+    }
+}
